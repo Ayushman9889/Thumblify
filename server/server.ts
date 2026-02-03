a2b007d8cebdb2823cf6038c6a3a6bbfdc51c612
Ayushman <ayushmanjaiswal199@gmail.com>
2026-02-04 00:13:00 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumblify-psi.vercel.app/'],
     credentials: true
 }))
 
